5d7aa358a34c7c63e6776fefe5e3024fb66c1173
Stéphane Bressani <stephane@bressani.dev>
2023-04-04 21:11:15 +0200

    meilleur présentation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from "axios"
 //import logo from './logo.svg'
 import './App.css'
+import 'bulma/css/bulma.min.css'
 import {match} from "assert"
 /*
 <img src={logo} className="App-logo" alt="logo" />
@@ -12,11 +13,8 @@ interface FormProps {
 }
 
 interface FormData {
-  year: number
-  month: number
-  day: number
-  hour: number
-  min: number
+  date: string
+  hourMin: string
   lat: number
   lng: number
   gmt: number
@@ -79,7 +77,7 @@ interface JsonCgiApi {
 
 function App() {
   const [formData, setFormData] = React.useState<FormData>({
-    year: 1984, month: 4, day: 1, hour: 0, min: 0, lat: 46.12, lng: 6.09, gmt: 2, color: 1,
+    date: "1984-04-01", hourMin: "00:00", lat: 46.12, lng: 6.09, gmt: 2, color: 1,
     aspect: 1,
     Soleil: true,
     Lune: true,
@@ -144,12 +142,14 @@ function App() {
     if (sw_debug) {
       url = "http://localhost:8888/"
     }
+    const hourMin = formData.hourMin.split(":")
+    const date = formData.date.split("-")
     url += "cgi-bin/SweInterface.cgi?sw_json=true" +
-        "&year=" + formData.year +
-        "&month=" + formData.month +
-        "&day=" + formData.day +
-        "&hour=" + formData.hour +
-        "&min=" + formData.min +
+        "&year=" + date[0] +
+        "&month=" + date[1] +
+        "&day=" + date[2] +
+        "&hour=" + hourMin[0] +
+        "&min=" + hourMin[1] +
         "&lat=" + formData.lat +
         "&lng=" + formData.lng +
         "&gmt=" + formData.gmt
@@ -1227,116 +1227,107 @@ function App() {
   } else if (formData.aspect == 2) {
       aspect_option = "&aspect_option=0,1,2,3,4,5,6,8,9,11,15,17,24,98,99"
   }
-
+  const hourMin = formData.hourMin.split(":")
+  const date = formData.date.split("-")
   return (
       <div className="App">
         <header className="App-header" style={{backgroundColor: formData.color == 0 ? "#ffffff" : "#282c34", color: formData.color == 0 ? "black" : "white"}}>
-        <div className="Chart" style={{backgroundImage: "url(\"https://astrologie-traditionnelle.net/cgi-bin/SweInterface.cgi?sw_chart=true" +
-              "&year=" + formData.year +
-              "&month=" + formData.month +
-              "&day=" + formData.day +
-              "&hour=" + formData.hour +
-              "&min=" + formData.min +
-              "&lat=" + formData.lat +
-              "&lng=" + formData.lng +
-              "&gmt=" + formData.gmt +
-              "&color=" + formData.color +
-              aspect_option + "\")"}}></div>
-        <table>
-          <tbody>
-          {bodiesTr}
-          </tbody>
-        </table>
         <p />
-        <table>
-          <tbody>
-          {aspectTr}
-          </tbody>
-        </table>
         <p />
         <form onSubmit={handleSubmit}>
-          <table>
-            <tbody>
-              <tr>
-                <td>
-                  <label>
-                    Année de naissance:
-                    <input type="number" name="year" value={formData.year} onChange={handleInputChange} />
-                  </label>
-                </td>
-                <td>
-                  <label>
-                    Mois de naissance:
-                    <input type="number" name="month" value={formData.month} onChange={handleInputChange} />
-                  </label>
-                </td>
-                <td>
-                  <label>
-                    Jour de naissance:
-                    <input type="number" name="day" value={formData.day} onChange={handleInputChange} />
-                  </label>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label>
-                    Heure de naissance:
-                    <input type="number" name="hour" value={formData.hour} onChange={handleInputChange} />
-                  </label>
-                </td>
-                <td>
-                  <label>
-                    Minute de naissance:
-                    <input type="number" name="min" value={formData.min} onChange={handleInputChange} />
-                  </label>
-                </td>
-                <td>
-                  <label>
-                    GMT à la naissance:
-                    <input type="number" name="gmt" value={formData.gmt} onChange={handleInputChange} />
-                  </label>
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <label>
-                    Latitude de naissance:
-                    <input type="number" name="lat" value={formData.lat} onChange={handleInputChange} />
-                  </label>
-                </td>
-                <td>
-                  <label>
-                    Longitude de naissance:
-                    <input type="number" name="lng" value={formData.lng} onChange={handleInputChange} />
-                  </label>
-                </td>
-                <td>
-                  <label>
-                    Couleur:
-                    <br />
-                    <select name="color" value={formData.color} onChange={handleSelectChange}>
-                      <option value="0">Clair</option>
-                      <option value="1">Sombre</option>
-                    </select>
-                  </label>
-                </td>
-              </tr>
-              <tr>
-                <td colSpan={2} />
-                <td>
-                  <label>
-                    Aspect:
-                    <br />
-                    <select name="aspect" value={formData.aspect} onChange={handleSelectChange}>
-                      <option value="0">Aucun</option>
-                      <option value="1">Soleil à Noeud Lunaire + Asc et Mc</option>
-                      <option value="2">Tous les aspects</option>
-                    </select>
-                  </label>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="columns">
+            <div className="column">
+              <fieldset className="FieldSet">
+                <legend className="subtitle is-3">Carte du ciel: Natal et Transit.</legend>
+                <div className="Chart" style={{backgroundImage: "url(\"https://astrologie-traditionnelle.net/cgi-bin/SweInterface.cgi?sw_chart=true" +
+                      "&year=" + date[0] +
+                      "&month=" + date[1] +
+                      "&day=" + date[2] +
+                      "&hour=" + hourMin[0] +
+                      "&min=" + hourMin[1] +
+                      "&lat=" + formData.lat +
+                      "&lng=" + formData.lng +
+                      "&gmt=" + formData.gmt +
+                      "&color=" + formData.color +
+                      aspect_option + "\")"}}></div>
+              </fieldset>
+            </div>
+            <div className="column">
+              <fieldset className="FieldSet">
+                <legend className="subtitle is-3">Naissance et options</legend>
+                <label>
+                  Date:
+                  <br />
+                  <input type="date" name="date" value={formData.date} onChange={handleInputChange} />
+                </label>
+                <br />
+                <label>
+                  Heure:
+                  <br />
+                  <input type="time" name="hourMin" value={formData.hourMin} onChange={handleInputChange} />
+                </label>
+                <br />
+                <label>
+                  GMT:
+                  <br />
+                  <input type="number" name="gmt" value={formData.gmt} onChange={handleInputChange} />
+                </label>
+                <br />
+                <label>
+                  Latitude:
+                  <br />
+                  <input type="number" name="lat" value={formData.lat} onChange={handleInputChange} />
+                </label>
+                <br />
+                <label>
+                  Longitude:
+                  <br />
+                  <input type="number" name="lng" value={formData.lng} onChange={handleInputChange} />
+                </label>
+                <br />
+                <label>
+                  Couleur:
+                  <br />
+                  <select name="color" value={formData.color} onChange={handleSelectChange}>
+                    <option value="0">Clair</option>
+                    <option value="1">Sombre</option>
+                  </select>
+                </label>
+                <br />
+                <label>
+                  Aspect:
+                  <br />
+                  <select name="aspect" value={formData.aspect} onChange={handleSelectChange}>
+                    <option value="0">Aucun</option>
+                    <option value="1">Soleil à Noeud Lunaire + Asc et Mc</option>
+                    <option value="2">Tous les aspects</option>
+                  </select>
+                </label>
+              </fieldset>
+            </div>
+          </div>
+          <div className="columns">
+            <div className="column">
+              <fieldset className="FieldSet">
+                <legend className="subtitle is-3">Aspects</legend>
+                <table>
+                  <tbody>
+                  {aspectTr}
+                  </tbody>
+                </table>
+              </fieldset>
+            </div>
+            <div className="column">
+              <fieldset className="FieldSet">
+                <legend className="subtitle is-3">Astres: Natal et Transit</legend>
+                <table>
+                  <tbody>
+                  {bodiesTr}
+                  </tbody>
+                </table>
+              </fieldset>
+            </div>
+          </div>
         </form>
       </header>
     </div>
